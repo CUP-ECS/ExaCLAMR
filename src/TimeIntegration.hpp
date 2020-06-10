08d54e311cd247715af1fa4d3ca514eb9a38db51
Jered Dominguez-Trujillo <jereddt@gmail.com>
2020-06-09 20:56:23 -0600

    Time Integration performance improvement - for 100 x 100 grid, 3000 iterations, went from 8.4 sec to 5.5 sec

--- src/TimeIntegration.hpp
+++ src/TimeIntegration.hpp
@@ -146,41 +146,41 @@ state_t setTimeStep( const ProblemManagerType& pm, const ExecutionSpace& exec_sp
 
 #define POW2( x ) ( ( x ) * ( x ) )
 
-#define HXRGFLUXIC ( uCurrent( i, j, k, 0 ) )
-#define HXRGFLUXNL ( uCurrent( i - 1, j, k, 0 ) )
-#define HXRGFLUXNR ( uCurrent( i + 1, j, k, 0 ) )
-#define HXRGFLUXNT ( uCurrent( i, j - 1, k, 0 ) )
-#define HXRGFLUXNB ( uCurrent( i, j + 1, k, 0 ) )
+#define HXRGFLUXIC ( uIC )
+#define HXRGFLUXNL ( uLeft )
+#define HXRGFLUXNR ( uRight )
+#define HXRGFLUXNT ( uBot )
+#define HXRGFLUXNB ( uTop )
 
-#define UXRGFLUXIC ( POW2( uCurrent( i, j, k, 0 ) )     / hCurrent( i, j, k, 0 )     + ghalf * POW2( hCurrent( i, j, k, 0 ) ) )
-#define UXRGFLUXNL ( POW2( uCurrent( i - 1, j, k, 0 ) ) / hCurrent( i - 1, j, k, 0 ) + ghalf * POW2( hCurrent( i - 1, j, k, 0 ) ) )
-#define UXRGFLUXNR ( POW2( uCurrent( i + 1, j, k, 0 ) ) / hCurrent( i + 1, j, k, 0 ) + ghalf * POW2( hCurrent( i + 1, j, k, 0 ) ) )
-#define UXRGFLUXNB ( POW2( uCurrent( i, j - 1, k, 0 ) ) / hCurrent( i, j - 1, k, 0 ) + ghalf * POW2( hCurrent( i, j - 1, k, 0 ) ) )
-#define UXRGFLUXNT ( POW2( uCurrent( i, j + 1, k, 0 ) ) / hCurrent( i, j + 1, k, 0 ) + ghalf * POW2( hCurrent( i, j + 1, k, 0 ) ) )
+#define UXRGFLUXIC ( POW2( uIC )    / hIC    + ghalf * POW2( hIC ) )
+#define UXRGFLUXNL ( POW2( uLeft )  / hLeft  + ghalf * POW2( hLeft ) )
+#define UXRGFLUXNR ( POW2( uRight ) / hRight + ghalf * POW2( hRight ) )
+#define UXRGFLUXNB ( POW2( uBot )   / hBot   + ghalf * POW2( hBot ) )
+#define UXRGFLUXNT ( POW2( uTop )   / hTop   + ghalf * POW2( hTop ) )
 
-#define VXRGFLUXIC ( uCurrent( i, j, k, 0 )     * uCurrent( i, j, k, 1 )     / hCurrent( i, j, k, 0 ) )
-#define VXRGFLUXNL ( uCurrent( i - 1, j, k, 0 ) * uCurrent( i - 1, j, k, 1 ) / hCurrent( i - 1, j, k, 0 ) )
-#define VXRGFLUXNR ( uCurrent( i + 1, j, k, 0 ) * uCurrent( i + 1, j, k, 1 ) / hCurrent( i + 1, j, k, 0 ) )
-#define VXRGFLUXNB ( uCurrent( i, j - 1, k, 0 ) * uCurrent( i, j - 1, k, 1 ) / hCurrent( i, j - 1, k, 0 ) )
-#define VXRGFLUXNT ( uCurrent( i, j + 1, k, 0 ) * uCurrent( i, j + 1, k, 1 ) / hCurrent( i, j + 1, k, 0 ) )
+#define VXRGFLUXIC ( uIC    * vIC    / hIC )
+#define VXRGFLUXNL ( uLeft  * vLeft  / hLeft )
+#define VXRGFLUXNR ( uRight * vRight / hRight )
+#define VXRGFLUXNB ( uBot   * vBot   / hBot )
+#define VXRGFLUXNT ( uTop   * vTop   / hTop )
 
-#define HYRGFLUXIC ( uCurrent( i, j, k, 1 ) )
-#define HYRGFLUXNL ( uCurrent( i - 1, j, k, 1 ) )
-#define HYRGFLUXNR ( uCurrent( i + 1, j, k, 1 ) )
-#define HYRGFLUXNB ( uCurrent( i, j - 1, k, 1 ) )
-#define HYRGFLUXNT ( uCurrent( i, j + 1, k, 1 ) )
+#define HYRGFLUXIC ( vIC )
+#define HYRGFLUXNL ( vLeft )
+#define HYRGFLUXNR ( vRight )
+#define HYRGFLUXNB ( vBot )
+#define HYRGFLUXNT ( vTop )
 
-#define UYRGFLUXIC ( uCurrent( i, j, k, 1 )     * uCurrent( i, j, k, 0 )     / hCurrent( i, j, k, 0 ) )
-#define UYRGFLUXNL ( uCurrent( i - 1, j, k, 1 ) * uCurrent( i - 1, j, k, 0 ) / hCurrent( i - 1, j, k, 0 ) )
-#define UYRGFLUXNR ( uCurrent( i + 1, j, k, 1 ) * uCurrent( i + 1, j, k, 0 ) / hCurrent( i + 1, j, k, 0 ) )
-#define UYRGFLUXNB ( uCurrent( i, j - 1, k, 1 ) * uCurrent( i, j - 1, k, 0 ) / hCurrent( i, j - 1, k, 0 ) )
-#define UYRGFLUXNT ( uCurrent( i, j + 1, k, 1 ) * uCurrent( i, j + 1, k, 0 ) / hCurrent( i, j + 1, k, 0 ) )
+#define UYRGFLUXIC ( vIC    * uIC    / hIC )
+#define UYRGFLUXNL ( vLeft  * uLeft  / hLeft )
+#define UYRGFLUXNR ( vRight * uRight / hRight )
+#define UYRGFLUXNB ( vBot   * uBot   / hBot )
+#define UYRGFLUXNT ( vTop   * uTop   / hTop )
 
-#define VYRGFLUXIC ( POW2( uCurrent( i, j, k, 1 ) )     / hCurrent( i, j, k, 0 )     + ghalf * POW2( hCurrent( i, j, k, 0 ) ) )
-#define VYRGFLUXNL ( POW2( uCurrent( i - 1, j, k, 1 ) ) / hCurrent( i - 1, j, k, 0 ) + ghalf * POW2( hCurrent( i - 1, j, k, 0 ) ) )
-#define VYRGFLUXNR ( POW2( uCurrent( i + 1, j, k, 1 ) ) / hCurrent( i + 1, j, k, 0 ) + ghalf * POW2( hCurrent( i + 1, j, k, 0 ) ) )
-#define VYRGFLUXNB ( POW2( uCurrent( i, j - 1, k, 1 ) ) / hCurrent( i, j - 1, k, 0 ) + ghalf * POW2( hCurrent( i, j - 1, k, 0 ) ) )
-#define VYRGFLUXNT ( POW2( uCurrent( i, j + 1, k, 1 ) ) / hCurrent( i, j + 1, k, 0 ) + ghalf * POW2( hCurrent( i, j + 1, k, 0 ) ) )
+#define VYRGFLUXIC ( POW2( vIC )    / hIC    + ghalf * POW2( hIC ) )
+#define VYRGFLUXNL ( POW2( vLeft )  / hLeft  + ghalf * POW2( hLeft ) )
+#define VYRGFLUXNR ( POW2( vRight ) / hRight + ghalf * POW2( hRight ) )
+#define VYRGFLUXNB ( POW2( vBot )   / hBot   + ghalf * POW2( hBot ) )
+#define VYRGFLUXNT ( POW2( vTop )   / hTop   + ghalf * POW2( hTop ) )
 
 template<typename state_t>
 inline state_t wCorrector( state_t dt, state_t dr, state_t uEigen, state_t gradHalf, state_t gradMinus, state_t gradPlus ) {
@@ -252,32 +252,62 @@ void step( const ProblemManagerType& pm, const ExecutionSpace& exec_space, const
         // Simple Diffusion Problem
         // hNew( i, j, k, 0 ) = ( hCurrent( i - 1, j, k, 0 ) + hCurrent( i + 1, j, k, 0 ) + hCurrent( i, j - 1, k, 0 ) + hCurrent( i, j + 1, k, 0 ) ) / 4;
 
-        
+        //
+        state_t hIC     = hCurrent( i, j, k, 0 );
+        state_t hLeft   = hCurrent( i - 1, j, k, 0 );
+        state_t hRight  = hCurrent( i + 1, j, k, 0 );
+        state_t hBot    = hCurrent( i, j - 1, k, 0 );
+        state_t hTop    = hCurrent( i, j + 1, k, 0 );
+        state_t hLeft2  = hCurrent( i - 2, j, k, 0 );
+        state_t hRight2 = hCurrent( i + 2, j, k, 0 );
+        state_t hBot2   = hCurrent( i, j - 2, k, 0 );
+        state_t hTop2   = hCurrent( i, j + 2, k, 0 );
+
+        state_t uIC     = uCurrent( i, j, k, 0 );
+        state_t uLeft   = uCurrent( i - 1, j, k, 0 );
+        state_t uRight  = uCurrent( i + 1, j, k, 0 );
+        state_t uBot    = uCurrent( i, j - 1, k, 0 );
+        state_t uTop    = uCurrent( i, j + 1, k, 0 );
+        state_t uLeft2  = uCurrent( i - 2, j, k, 0 );
+        state_t uRight2 = uCurrent( i + 2, j, k, 0 );
+        state_t uBot2   = uCurrent( i, j - 2, k, 0 );
+        state_t uTop2   = uCurrent( i, j + 2, k, 0 );
+
+        state_t vIC     = uCurrent( i, j, k, 1 );
+        state_t vLeft   = uCurrent( i - 1, j, k, 1 );
+        state_t vRight  = uCurrent( i + 1, j, k, 1 );
+        state_t vBot    = uCurrent( i, j - 1, k, 1 );
+        state_t vTop    = uCurrent( i, j + 1, k, 1 );
+        state_t vLeft2  = uCurrent( i - 2, j, k, 1 );
+        state_t vRight2 = uCurrent( i + 2, j, k, 1 );
+        state_t vBot2   = uCurrent( i, j - 2, k, 1 );
+        state_t vTop2   = uCurrent( i, j + 2, k, 1 );
+
         // Shallow Water Equations
-        state_t HxMinus = 0.5 * ( ( hCurrent( i - 1, j, k, 0 ) + hCurrent( i, j, k, 0 ) ) - ( dt ) / ( dx ) * ( ( HXRGFLUXIC ) - ( HXRGFLUXNL ) ) );
-        state_t UxMinus = 0.5 * ( ( uCurrent( i - 1, j, k, 0 ) + uCurrent( i, j, k, 0 ) ) - ( dt ) / ( dx ) * ( ( UXRGFLUXIC ) - ( UXRGFLUXNL ) ) );
-        state_t VxMinus = 0.5 * ( ( uCurrent( i - 1, j, k, 1 ) + uCurrent( i, j, k, 1 ) ) - ( dt ) / ( dx ) * ( ( VXRGFLUXIC ) - ( VXRGFLUXNL ) ) );
+        state_t HxMinus = 0.5 * ( ( hLeft + hIC ) - ( dt ) / ( dx ) * ( ( HXRGFLUXIC ) - ( HXRGFLUXNL ) ) );
+        state_t UxMinus = 0.5 * ( ( uLeft + uIC ) - ( dt ) / ( dx ) * ( ( UXRGFLUXIC ) - ( UXRGFLUXNL ) ) );
+        state_t VxMinus = 0.5 * ( ( vLeft + vIC ) - ( dt ) / ( dx ) * ( ( VXRGFLUXIC ) - ( VXRGFLUXNL ) ) );
 
         if ( DEBUG ) std::cout << std::left << std::setw(10) << "HxMinus: " << std::setw(6) << HxMinus << \
         "\tUxMinus: " << std::setw(6) << UxMinus << "\tVxMinus: " << std::setw(6) << VxMinus << "\ti: " << i << "\tj: "<< j << "\tk: " << k << "\n";
 
-        state_t HxPlus  = 0.5 * ( ( hCurrent( i, j, k, 0 ) + hCurrent( i + 1, j, k, 0 ) ) - ( dt ) / ( dx ) * ( ( HXRGFLUXNR ) - ( HXRGFLUXIC ) ) );
-        state_t UxPlus  = 0.5 * ( ( uCurrent( i, j, k, 0 ) + uCurrent( i + 1, j, k, 0 ) ) - ( dt ) / ( dx ) * ( ( UXRGFLUXNR ) - ( UXRGFLUXIC ) ) );
-        state_t VxPlus  = 0.5 * ( ( uCurrent( i, j, k, 1 ) + uCurrent( i + 1, j, k, 1 ) ) - ( dt ) / ( dx ) * ( ( VXRGFLUXNR ) - ( VXRGFLUXIC ) ) );
+        state_t HxPlus  = 0.5 * ( ( hIC + hRight ) - ( dt ) / ( dx ) * ( ( HXRGFLUXNR ) - ( HXRGFLUXIC ) ) );
+        state_t UxPlus  = 0.5 * ( ( uIC + uRight ) - ( dt ) / ( dx ) * ( ( UXRGFLUXNR ) - ( UXRGFLUXIC ) ) );
+        state_t VxPlus  = 0.5 * ( ( vIC + vRight ) - ( dt ) / ( dx ) * ( ( VXRGFLUXNR ) - ( VXRGFLUXIC ) ) );
 
         if ( DEBUG ) std::cout << std::left << std::setw(10) << "HxPlus: " << std::setw(6) << HxPlus << \
         "\tUxPlus: " << std::setw(6) << UxPlus << "\tVxPlus: " << std::setw(6) << VxPlus << "\ti: " << i << "\tj: " << j << "\tk: " << k << "\n";
 
-        state_t HyMinus = 0.5 * ( ( hCurrent( i, j - 1, k, 0 ) + hCurrent( i, j, k, 0 ) ) - ( dt ) / ( dy ) * ( ( HYRGFLUXIC ) - ( HYRGFLUXNB ) ) );
-        state_t UyMinus = 0.5 * ( ( uCurrent( i, j - 1, k, 0 ) + uCurrent( i, j, k, 0 ) ) - ( dt ) / ( dy ) * ( ( UYRGFLUXIC ) - ( UYRGFLUXNB ) ) );
-        state_t VyMinus = 0.5 * ( ( uCurrent( i, j - 1, k, 1 ) + uCurrent( i, j, k, 1 ) ) - ( dt ) / ( dy ) * ( ( VYRGFLUXIC ) - ( VYRGFLUXNB ) ) );
+        state_t HyMinus = 0.5 * ( ( hBot + hIC ) - ( dt ) / ( dy ) * ( ( HYRGFLUXIC ) - ( HYRGFLUXNB ) ) );
+        state_t UyMinus = 0.5 * ( ( uBot + uIC ) - ( dt ) / ( dy ) * ( ( UYRGFLUXIC ) - ( UYRGFLUXNB ) ) );
+        state_t VyMinus = 0.5 * ( ( vBot + vIC ) - ( dt ) / ( dy ) * ( ( VYRGFLUXIC ) - ( VYRGFLUXNB ) ) );
 
         if ( DEBUG ) std::cout << std::left << std::setw(10) << "HyMinus: " << std::setw(6) << HyMinus << \
         "\tUyMinus: " << std::setw(6) << UyMinus << "\tVyMinus: " << std::setw(6) << VyMinus << "\ti: " << i << "\tj: " << j << "\tk: " << k << "\n";
 
-        state_t HyPlus  = 0.5 * ( ( hCurrent( i, j, k, 0 ) + hCurrent( i, j + 1, k, 0 ) ) - ( dt ) / ( dy ) * ( ( HYRGFLUXNT ) - ( HYRGFLUXIC ) ) );
-        state_t UyPlus  = 0.5 * ( ( uCurrent( i, j, k, 0 ) + uCurrent( i, j + 1, k, 0 ) ) - ( dt ) / ( dy ) * ( ( UYRGFLUXNT ) - ( UYRGFLUXIC ) ) );
-        state_t VyPlus  = 0.5 * ( ( uCurrent( i, j, k, 1 ) + uCurrent( i, j + 1, k, 1 ) ) - ( dt ) / ( dy ) * ( ( VYRGFLUXNT ) - ( VYRGFLUXIC ) ) );
+        state_t HyPlus  = 0.5 * ( ( hIC + hTop ) - ( dt ) / ( dy ) * ( ( HYRGFLUXNT ) - ( HYRGFLUXIC ) ) );
+        state_t UyPlus  = 0.5 * ( ( uIC + uTop ) - ( dt ) / ( dy ) * ( ( UYRGFLUXNT ) - ( UYRGFLUXIC ) ) );
+        state_t VyPlus  = 0.5 * ( ( vIC + vTop ) - ( dt ) / ( dy ) * ( ( VYRGFLUXNT ) - ( VYRGFLUXIC ) ) );
 
         if ( DEBUG ) std::cout << std::left << std::setw(10) << "HyPlus: " << std::setw(6) << HyPlus << \
         "\tUyPlus: " << std::setw(6) << UyPlus << "\tVyPlus: " << std::setw(6) << VyPlus << "\ti: " << i << "\tj: " << j << "\tk: " << k << "\n";
@@ -299,35 +329,35 @@ void step( const ProblemManagerType& pm, const ExecutionSpace& exec_space, const
         UyFluxPlus( i, j, k, 1 ) = ( POW2( VyPlus ) / HyPlus + ghalf * POW2( HyPlus ) );
 
 
-        HxWMinus( i, j, k, 0 ) = wCorrector( dt, dx, fabs( UxMinus / HxMinus ) + sqrt( gravity * HxMinus ), hCurrent( i, j, k, 0 ) - hCurrent( i - 1, j, k, 0 ), hCurrent( i - 1, j, k, 0 ) - hCurrent( i - 2, j, k, 0 ), hCurrent( i + 1, j, k, 0 ) - hCurrent( i, j, k, 0 ) );
-        HxWMinus( i, j, k, 0 ) *= hCurrent( i, j, k, 0 ) - hCurrent( i - 1, j, k, 0 );
+        HxWMinus( i, j, k, 0 ) = wCorrector( dt, dx, fabs( UxMinus / HxMinus ) + sqrt( gravity * HxMinus ), hIC - hLeft, hLeft - hLeft2, hRight - hIC );
+        HxWMinus( i, j, k, 0 ) *= hIC - hLeft;
         
-        HxWPlus( i, j, k, 0 )  = wCorrector( dt, dx, fabs( UxPlus / HxPlus ) + sqrt( gravity * HxPlus ), hCurrent( i + 1, j, k, 0 ) - hCurrent( i, j, k, 0 ), hCurrent( i, j, k, 0 ) - hCurrent( i - 1, j, k, 0 ), hCurrent( i + 2, j, k, 0 ) - hCurrent( i + 1, j, k, 0 ) );
-        HxWPlus( i, j, k, 0 )  *= hCurrent( i + 1, j, k, 0 ) - hCurrent( i, j, k, 0 );
+        HxWPlus( i, j, k, 0 )  = wCorrector( dt, dx, fabs( UxPlus / HxPlus ) + sqrt( gravity * HxPlus ), hRight - hIC, hIC - hLeft, hRight2 - hRight );
+        HxWPlus( i, j, k, 0 )  *= hRight - hIC;
 
-        UWMinus( i, j, k, 0 )  = wCorrector( dt, dx, fabs( UxMinus / HxMinus ) + sqrt( gravity * HxMinus ), uCurrent( i, j, k, 0 ) - uCurrent( i - 1, j, k, 0 ), uCurrent( i - 1, j, k, 0 ) - uCurrent( i - 2, j, k, 0 ), uCurrent( i + 1, j, k, 0 ) - uCurrent( i, j, k, 0 ) );
-        UWMinus( i, j, k, 0 )  *= uCurrent( i, j, k, 0 ) - uCurrent( i - 1, j, k, 0 );
+        UWMinus( i, j, k, 0 )  = wCorrector( dt, dx, fabs( UxMinus / HxMinus ) + sqrt( gravity * HxMinus ), uIC - uLeft, uLeft - uLeft2, uRight - uIC );
+        UWMinus( i, j, k, 0 )  *= uIC - uLeft;
 
-        UWPlus( i, j, k, 0 )   = wCorrector( dt, dx, fabs( UxPlus / HxPlus ) + sqrt( gravity * HxPlus ), uCurrent( i + 1, j, k, 0 ) - uCurrent( i, j, k, 0 ), uCurrent( i, j, k, 0 ) - uCurrent( i - 1, j, k, 0 ), uCurrent( i + 2, j, k, 0 ) - uCurrent( i + 1, j, k, 0 ) );
-        UWPlus( i, j, k, 0 )   *= uCurrent( i + 1, j, k, 0 ) - uCurrent( i, j, k, 0 );
-
-
-        HyWMinus( i, j, k, 0 ) = wCorrector( dt, dy, fabs( VyMinus / HyMinus ) + sqrt( gravity * HyMinus ), hCurrent( i, j, k, 0) - hCurrent( i, j - 1, k, 0 ), hCurrent( i, j - 1, k, 0 ) - hCurrent( i, j - 2, k, 0 ), hCurrent( i, j + 1, k, 0 ) - hCurrent( i, j, k, 0) );
-        HyWMinus( i, j, k, 0 ) *= hCurrent( i, j, k, 0 ) - hCurrent( i, j - 1, k, 0 );
-
-        HyWPlus( i, j, k, 0 )  = wCorrector( dt, dy, fabs( VyPlus / HyPlus ) + sqrt( gravity * HyPlus ), hCurrent( i, j + 1, k, 0) - hCurrent( i, j, k, 0 ), hCurrent( i, j, k, 0 ) - hCurrent( i, j - 1, k, 0 ), hCurrent( i, j + 2, k, 0 ) - hCurrent( i, j + 1, k, 0)  );
-        HyWPlus( i, j, k, 0 )  *= hCurrent( i, j + 1, k, 0 ) - hCurrent( i, j, k, 0 );
-
-        UWMinus( i, j, k, 1 )  = wCorrector( dt, dy, fabs( VyMinus / HyMinus ) + sqrt( gravity * HyMinus ), uCurrent( i, j, k, 1 ) - uCurrent( i, j - 1, k, 1 ), uCurrent( i, j - 1, k, 1) - uCurrent( i, j - 2, k, 1 ), uCurrent( i, j + 1, k, 1 ) - uCurrent( i, j, k, 1 ) );
-        UWMinus( i, j, k, 1 )  *= uCurrent( i, j, k, 1 ) - uCurrent( i, j - 1, k, 1 );
-
-        UWPlus( i, j, k, 1 )   = wCorrector( dt, dy, fabs( VyPlus / HyPlus ) + sqrt( gravity * HyPlus ), uCurrent( i, j + 1, k, 1 ) - uCurrent( i, j, k, 1 ), uCurrent( i, j, k, 1) - uCurrent( i, j - 1, k, 1 ), uCurrent( i, j + 2, k, 1 ) - uCurrent( i, j + 1, k, 1 ) );
-        UWPlus( i, j, k, 1 )   *= uCurrent( i, j + 1, k, 1 ) - uCurrent( i, j, k, 1 );
+        UWPlus( i, j, k, 0 )   = wCorrector( dt, dx, fabs( UxPlus / HxPlus ) + sqrt( gravity * HxPlus ), uRight - uIC, uIC - uLeft, uRight2 - uRight );
+        UWPlus( i, j, k, 0 )   *= uRight - uIC;
 
 
-        hNew ( i, j, k, 0 ) = uFullStep( dt, dx, hCurrent( i, j, k, 0 ), HxFluxPlus( i, j, k, 0 ), HxFluxMinus( i, j, k, 0 ), HyFluxPlus( i, j, k, 0 ), HyFluxMinus( i, j, k, 0 ) ) - HxWMinus( i, j, k, 0 ) + HxWPlus( i, j, k, 0 ) - HyWMinus( i, j, k, 0 ) + HyWPlus( i, j, k, 0 );
-        uNew ( i, j, k, 0 ) = uFullStep( dt, dx, uCurrent( i, j, k, 0 ), UxFluxPlus( i, j, k, 0 ), UxFluxMinus( i, j, k, 0 ), UyFluxPlus( i, j, k, 0 ), UyFluxMinus( i, j, k, 0 ) ) - UWMinus( i, j, k, 0 ) + UWPlus( i, j, k, 0 );
-        uNew ( i, j, k, 1 ) = uFullStep( dt, dy, uCurrent( i, j, k, 1 ), UxFluxPlus( i, j, k, 1 ), UxFluxMinus( i, j, k, 1 ), UyFluxPlus( i, j, k, 1 ), UyFluxMinus( i, j, k, 1 ) ) - UWMinus( i, j, k, 1 ) + UWPlus( i, j, k, 1 );
+        HyWMinus( i, j, k, 0 ) = wCorrector( dt, dy, fabs( VyMinus / HyMinus ) + sqrt( gravity * HyMinus ), hIC - hBot, hBot - hBot2, hTop - hIC );
+        HyWMinus( i, j, k, 0 ) *= hIC - hBot;
+
+        HyWPlus( i, j, k, 0 )  = wCorrector( dt, dy, fabs( VyPlus / HyPlus ) + sqrt( gravity * HyPlus ), hTop - hIC, hIC - hBot, hTop2 - hTop );
+        HyWPlus( i, j, k, 0 )  *= hTop - hIC;
+
+        UWMinus( i, j, k, 1 )  = wCorrector( dt, dy, fabs( VyMinus / HyMinus ) + sqrt( gravity * HyMinus ), vIC - vBot, vBot - vBot2, vTop - vIC );
+        UWMinus( i, j, k, 1 )  *= vIC - vBot;
+
+        UWPlus( i, j, k, 1 )   = wCorrector( dt, dy, fabs( VyPlus / HyPlus ) + sqrt( gravity * HyPlus ), vTop - vIC, vIC - vBot, vTop2 - vTop );
+        UWPlus( i, j, k, 1 )   *= vTop - vIC;
+
+
+        hNew ( i, j, k, 0 ) = uFullStep( dt, dx, hIC, HxFluxPlus( i, j, k, 0 ), HxFluxMinus( i, j, k, 0 ), HyFluxPlus( i, j, k, 0 ), HyFluxMinus( i, j, k, 0 ) ) - HxWMinus( i, j, k, 0 ) + HxWPlus( i, j, k, 0 ) - HyWMinus( i, j, k, 0 ) + HyWPlus( i, j, k, 0 );
+        uNew ( i, j, k, 0 ) = uFullStep( dt, dx, uIC, UxFluxPlus( i, j, k, 0 ), UxFluxMinus( i, j, k, 0 ), UyFluxPlus( i, j, k, 0 ), UyFluxMinus( i, j, k, 0 ) ) - UWMinus( i, j, k, 0 ) + UWPlus( i, j, k, 0 );
+        uNew ( i, j, k, 1 ) = uFullStep( dt, dy, vIC, UxFluxPlus( i, j, k, 1 ), UxFluxMinus( i, j, k, 1 ), UyFluxPlus( i, j, k, 1 ), UyFluxMinus( i, j, k, 1 ) ) - UWMinus( i, j, k, 1 ) + UWPlus( i, j, k, 1 );
        
     } );
 
