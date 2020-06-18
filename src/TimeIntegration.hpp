bad969390b26683331a816f53f6c11ba2b2981e0
Jered Dominguez-Trujillo <jereddt@gmail.com>
2020-06-18 12:00:26 -0600

    Cuda + MPI was having Mass Conservation issues on large grids - swapped Cajita Gather routine with my hand-written Halo Exchange for now

--- src/TimeIntegration.hpp
+++ src/TimeIntegration.hpp
@@ -111,10 +111,95 @@ void haloExchange( const ProblemManagerType& pm, const int time_step ) {
     // DEBUG: Trace in Halo Exchange
     if ( pm.mesh()->rank() == 0 && DEBUG ) std::cout << "Starting Halo Exchange\n";
 
+    /*
     // Perform Halo Exchange on Height and Momentum State Views
     pm.gather( Location::Cell(), Field::Height(), NEWFIELD( time_step ) );
     pm.gather( Location::Cell(), Field::Momentum(), NEWFIELD( time_step ) );
+    */
+    auto local_grid = pm.mesh()->localGrid();
+    auto owned_cells = local_grid->indexSpace( Cajita::Own(), Cajita::Cell(), Cajita::Local() );
 
+    auto uCurrent = pm.get( Location::Cell(), Field::Momentum(), CURRENTFIELD( time_step ) );
+    auto hCurrent = pm.get( Location::Cell(), Field::Height(), CURRENTFIELD( time_step ) );
+
+    auto uNew = pm.get( Location::Cell(), Field::Momentum(), NEWFIELD( time_step ) );
+    auto hNew = pm.get( Location::Cell(), Field::Height(), NEWFIELD( time_step ) );
+
+    for ( int i = -1; i < 2; i++ ) {
+        for (int j = -1; j < 2; j++) {
+            if ( ( i == 0 || j == 0 ) && !( i == 0 && j == 0 ) ){
+                int neighbor = local_grid->neighborRank( i, j, 0 );
+                if ( neighbor != -1 ) {
+                    auto shared_recv_cells = local_grid->sharedIndexSpace( Cajita::Ghost(), Cajita::Cell(), i, j, 0 );
+                    auto shared_send_cells = local_grid->sharedIndexSpace( Cajita::Own(), Cajita::Cell(), i, j, 0 );
+
+                    
+                    if ( DEBUG ) std::cout << "Rank: " << pm.mesh()->rank() << "\t i: " << i << "\tj: " << j << "\tk: " << 0 << "\tNeighbor: " << neighbor << "\n";
+                    if ( DEBUG ) std::cout << "Rank (Recv): " << pm.mesh()->rank() << "\txmin: " << shared_recv_cells.min( 0 ) << "\txmax: " << shared_recv_cells.max( 0 ) \
+                    << "\tymin: " << shared_recv_cells.min( 1 ) << "\tymax: " << shared_recv_cells.max( 1 ) << "\tzmin: " << shared_recv_cells.min( 2 ) << "\tzmax: " << shared_recv_cells.max( 2 ) << "\n";
+                    if ( DEBUG ) std::cout << "Rank (Send): " << pm.mesh()->rank() << "\txmin: " << shared_send_cells.min( 0 ) << "\txmax: " << shared_send_cells.max( 0 ) \
+                    << "\tymin: " << shared_send_cells.min( 1 ) << "\tymax: " << shared_send_cells.max( 1 ) << "\tzmin: " << shared_send_cells.min( 2 ) << "\tzmax: " << shared_send_cells.max( 2 ) << "\n";
+
+                    double sendH[shared_send_cells.size()];
+                    double sendU[shared_send_cells.size()];
+                    double sendV[shared_send_cells.size()];
+                    double recvH[shared_recv_cells.size()];
+                    double recvU[shared_recv_cells.size()];
+                    double recvV[shared_recv_cells.size()];
+
+                    for ( int ii = shared_send_cells.min( 0 ); ii < shared_send_cells.max( 0 ); ii++ ) {
+                        for ( int jj = shared_send_cells.min( 1 ); jj < shared_send_cells.max( 1 ); jj++ ) {
+                            for ( int kk = shared_send_cells.min( 2 ); kk < shared_send_cells.max( 2 ); kk++ ) {
+                                int ii_own = ii - shared_send_cells.min( 0 );
+                                int jj_own = jj - shared_send_cells.min( 1 );
+                                int kk_own = kk - shared_send_cells.min( 2 );
+
+                                int inx = ii_own + shared_send_cells.extent( 0 ) * ( jj_own + shared_send_cells.extent( 1 ) * kk_own );
+
+                                // printf( "ii: %d\tjj: %d\tkk: %d\t ii_own: %d\tjj_own: %d\tkk_own: %d\tinx: %d\n", ii, jj, kk, ii_own, jj_own, kk_own, inx );
+
+                                sendH[inx] = hNew( ii, jj, kk, 0 );
+                                sendU[inx] = uNew( ii, jj, kk, 0 );
+                                sendV[inx] = uNew( ii, jj, kk, 1 );
+                            }
+                        }
+                    }
+
+                    MPI_Request request[6];
+                    MPI_Status statuses[6];
+
+                    MPI_Isend( sendH, shared_send_cells.size(), MPI_DOUBLE, neighbor, 0, MPI_COMM_WORLD, &request[0] );
+                    MPI_Isend( sendU, shared_send_cells.size(), MPI_DOUBLE, neighbor, 0, MPI_COMM_WORLD, &request[1] );
+                    MPI_Isend( sendV, shared_send_cells.size(), MPI_DOUBLE, neighbor, 0, MPI_COMM_WORLD, &request[2] );
+
+                    MPI_Irecv( recvH, shared_recv_cells.size(), MPI_DOUBLE, neighbor, 0, MPI_COMM_WORLD, &request[3] );
+                    MPI_Irecv( recvU, shared_recv_cells.size(), MPI_DOUBLE, neighbor, 0, MPI_COMM_WORLD, &request[4] );
+                    MPI_Irecv( recvV, shared_recv_cells.size(), MPI_DOUBLE, neighbor, 0, MPI_COMM_WORLD, &request[5] );
+
+                    MPI_Waitall( 6, request, statuses );
+                    
+                    for ( int ii = shared_recv_cells.min( 0 ); ii < shared_recv_cells.max( 0 ); ii++ ) {
+                        for ( int jj = shared_recv_cells.min( 1 ); jj < shared_recv_cells.max( 1 ); jj++ ) {
+                            for ( int kk = shared_recv_cells.min( 2 ); kk < shared_recv_cells.max( 2 ); kk++ ) {
+                                int ii_own = ii - shared_recv_cells.min( 0 );
+                                int jj_own = jj - shared_recv_cells.min( 1 );
+                                int kk_own = kk - shared_recv_cells.min( 2 );
+
+                                int inx = ii_own + shared_recv_cells.extent( 0 ) * ( jj_own + shared_recv_cells.extent( 1 ) * kk_own );
+
+                                // printf( "Rank: %d\tii: %d\tjj: %d\tkk: %d\t ii_own: %d\tjj_own: %d\tkk_own: %d\tinx: %d\trecvH: %.4f\n", \
+                                pm.mesh()->rank(), ii, jj, kk, ii_own, jj_own, kk_own, inx, recvH[inx] );
+
+                                hNew( ii, jj, kk, 0 ) = recvH[inx];
+                                uNew( ii, jj, kk, 0 ) = recvU[inx];
+                                uNew( ii, jj, kk, 1 ) = recvV[inx];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     MPI_Barrier( MPI_COMM_WORLD );
 }
 
