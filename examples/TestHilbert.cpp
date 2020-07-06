1a5eb7b98042d65bb187771beb1a90f7311a9816
Jered Dominguez-Trujillo <jereddt@gmail.com>
2020-07-06 16:16:06 -0600

    Moved map to a Kokkos View rather than std::vector and finally got subview to compile with LayoutHilbert

--- examples/TestHilbert.cpp
+++ examples/TestHilbert.cpp
@@ -1,22 +1,57 @@
 #include <Kokkos_Core.hpp>
 #include <Hilbert.hpp>
+#include <Cabana_Core.hpp>
+#include <Cajita.hpp>
+
+#include <mpi.h>
 
 int main( int argc, char* argv[] ) {
+    MPI_Init( &argc, &argv );
     Kokkos::initialize( argc, argv );
     {
     std::cout << "Testing Hilbert Layout\n"; 
 
     std::cout << "HilbertArray\n";
-    Kokkos::View<double**, Kokkos::LayoutHilbert, Kokkos::HostSpace> HilbertArray( "Hilbert", 127, 35 );
+    Kokkos::View<double**, Kokkos::LayoutHilbert, Kokkos::HostSpace> HilbertArray( "Hilbert", 6, 4 );
 
-    for ( int i = 0; i < 18; i++ ) {
-        for ( int j = 0; j < 18; j++ ) {
+    for ( int i = 0; i < 6; i++ ) {
+        for ( int j = 0; j < 4; j++ ) {
             std::cout << "i: " << i << "\tj: " << j << "\tFlat Index: " << HilbertArray( i, j ) << "\n";
         }
     }
+
+    auto s = Kokkos::subview( HilbertArray, 3, 3 );
+
+    // using cell_array = Cajita::Array<double, Cajita::Cell, Cajita::UniformMesh<double>, Kokkos::LayoutHilbert>;
+
+    // std::array<double, 3> global_low_corner;
+    // global_low_corner[0] = 0.0;
+    // global_low_corner[1] = 0.0;
+    // global_low_corner[2] = 0.0;
+
+    // std::array<double, 3> global_high_corner;
+    // global_high_corner[0] = 1.0;
+    // global_high_corner[1] = 1.0;
+    // global_high_corner[2] = 1.0;
+
+    // std::array<double, 3> cell_size;
+    // cell_size[0] = 0.25;
+    // cell_size[1] = 0.25;
+    // cell_size[2] = 0.25;
+
+    // Cajita::ManualPartitioner partitioner( { 1, 1, 1 } );
+
+    // auto global_mesh = Cajita::createUniformGlobalMesh( global_low_corner, global_high_corner, cell_size );
+    // auto global_grid = Cajita::createGlobalGrid( MPI_COMM_WORLD, global_mesh, { false, false, false }, partitioner );
+    // auto local_grid = Cajita::createLocalGrid( global_grid, 2 );
+    // auto cell_scalar_layout = Cajita::createArrayLayout( local_grid, 1, Cajita::Cell() );
+
+    // std::shared_ptr<cell_array> height = Cajita::createArray<double, Kokkos::LayoutHilbert> ( "height", cell_scalar_layout );
+    // std::shared_ptr<cell_array> height = Cajita::createArray<double>( cell_scalar_layout, HilbertArray );
     
     }
     Kokkos::finalize();
+    MPI_Finalize();
 
     return 0;
 };
