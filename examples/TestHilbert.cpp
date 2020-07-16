d9a4ba0c89b05882fa27c0ba0329741a2b329576
Jered Dominguez-Trujillo <jereddt@gmail.com>
2020-07-15 22:00:46 -0600

    Hilbert Cabana License Header

--- examples/TestHilbert.cpp
+++ examples/TestHilbert.cpp
@@ -70,12 +70,12 @@ int main( int argc, char* argv[] ) {
     space = Cajita::IndexSpace<4>( { 0, 0, 0, 0 }, { 2, dim2, dim3, dim4 } );
 
     auto HilbertSub = Kokkos::subview( HilbertArray, space.range( 0 ), space.range( 1 ), space.range( 2 ), space.range( 3 ) );
-    auto RegularSub = Kokkos::subview( RegularArray, space.range( 0 ), space.range( 1 ), space.range( 2 ), space.range( 3 )  );
+    auto RegularSub = Kokkos::subview( RegularArray, space.range( 0 ), space.range( 1 ), space.range( 2 ), space.range( 3 ) );
 
     std::cout << "Hilbert SubView Var 1\n";
 
-    for ( int i = 0; i < 2; i++ ) {
-        for ( int j = 0; j < dim2; j++ ) {
+    for ( int i = space.min( 0 ); i < space.max( 0 ); i++ ) {
+        for ( int j = space.min( 1 ); j < space.max( 1 ); j++ ) {
             std::cout << HilbertSub( i, j, 0, 0 ) << " ";
         }
         std::cout << "\n";
@@ -83,8 +83,8 @@ int main( int argc, char* argv[] ) {
 
     std::cout << "Hilbert SubView Var 2\n";
 
-    for ( int i = 0; i < 2; i++ ) {
-        for ( int j = 0; j < dim2; j++ ) {
+    for ( int i = space.min( 0 ); i < space.max( 0 ); i++ ) {
+        for ( int j = space.min( 1 ); j < space.max( 1 ); j++ ) {
             std::cout << HilbertSub( i, j, 0, 1 ) << " ";
         }
         std::cout << "\n";
@@ -92,8 +92,8 @@ int main( int argc, char* argv[] ) {
 
     std::cout << "Regular SubView Var 1\n";
 
-    for ( int i = 0; i < 2; i++ ) {
-        for ( int j = 0; j < dim2; j++ ) {
+    for ( int i = space.min( 0 ); i < space.max( 0 ); i++ ) {
+        for ( int j = space.min( 1 ); j < space.max( 1 ); j++ ) {
             std::cout << RegularSub( i, j, 0, 0 ) << " ";
         }
         std::cout << "\n";
@@ -101,18 +101,20 @@ int main( int argc, char* argv[] ) {
 
     std::cout << "Regular SubView Var 2\n";
 
-    for ( int i = 0; i < 2; i++ ) {
-        for ( int j = 0; j < dim2; j++ ) {
+    for ( int i = space.min( 0 ); i < space.max( 0 ); i++ ) {
+        for ( int j = space.min( 1 ); j < space.max( 1 ); j++ ) {
             std::cout << RegularSub( i, j, 0, 1 ) << " ";
         }
         std::cout << "\n";
     }
 
-    Kokkos::View<double****> RegularSmall( "Regular", 2, dim2, dim3, dim4 );
-    for ( int i = 0; i < 2; i++ ) {
-        for ( int j = 0; j < dim2; j++ ) {
-            for ( int k = 0; k < dim3; k++ ) {
-                for ( int l = 0; l < dim4; l++ ) {
+    Kokkos::View<double****> RegularSmall( "RegularSmall", space.extent( 0 ), space.extent( 1 ), space.extent( 2 ), space.extent( 3 ) );
+
+    int replaceVal = 7012;
+    for ( int i = space.min( 0 ); i < space.max( 0 ); i++ ) {
+        for ( int j = space.min( 1 ); j < space.max( 1 ); j++ ) {
+            for ( int k = space.min( 2 ); k < space.max( 2 ); k++ ) {
+                for ( int l = space.min( 3 ); l < space.max( 3 ); l++ ) {
                     RegularSmall( i, j, k, l ) = 7012;
                 }
             }
@@ -137,6 +139,18 @@ int main( int argc, char* argv[] ) {
             std::cout << HilbertArray( i, j, 0, 1 ) << "\t";
         }
         std::cout << "\n";
+    }
+
+    std::cout << "Replacement Value\n";
+
+    for ( int i = space.min( 0 ); i < space.max( 0 ); i++ ) {
+        for ( int j = space.min( 1 ); j < space.max( 1 ); j++ ) {
+            for ( int k = space.min( 2 ); k < space.max( 2 ); k++ ) {
+                for ( int l = space.min( 3 ); l < space.max( 3 ); l++ ) {
+                    std::cout << HilbertArray( i, j, k, l ) << "\n";
+                }
+            }
+        }
     }
 
     }
