5ef401f2aa298733bcb3cafffc8f4a6d36861969
Jered Dominguez-Trujillo <jereddt@gmail.com>
2020-07-16 18:54:52 -0600

    Fixed build on my Mac

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,6 +27,6 @@ done
 rm -rf build
 mkdir -p build
 cd build
-cmake -D CMAKE_PREFIX_PATH=/usr/local -D CMAKE_C_COMPILER=/usr/local/Cellar/gcc/9.3.0_1/bin/gcc-9 -D CMAKE_CXX_COMPILER=/usr/local/Cellar/gcc/9.3.0_1/bin/g++-9 -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On ..
+cmake -D CMAKE_PREFIX_PATH=/usr/local -D CMAKE_C_COMPILER=/usr/local/bin/gcc-9 -D CMAKE_CXX_COMPILER=/usr/local/bin/g++-9 -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On ..
 # cmake -D CMAKE_C_COMPILER=/usr/local/bin/gcc-8 -D CMAKE_CXX_COMPILER=/usr/local/bin/g++-8 -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On ..
 make -j 8
