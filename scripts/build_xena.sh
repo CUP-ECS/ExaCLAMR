abfcb5ecac8435617b881c381949ac34ea6b6b51
Jered Dominguez-Trujillo <jereddt@gmail.com>
2020-09-23 14:45:23 -0600

    Updated Xena build

--- scripts/build_xena.sh
+++ scripts/build_xena.sh
@@ -2,22 +2,22 @@
 
 # Define Variables
 MYDIR=`pwd`
-INSTALL_DIR=~/xena-scratch/tmp
+INSTALL_DIR=~/xena-scratch/ExaCLAMR_Xena
 NVCC_CXX=${MYDIR}/libs/kokkos/bin/nvcc_wrapper
 KOKKOS_GIT=https://github.com/kokkos/kokkos.git
-KOKKOS_HASH=785d19f
-#CABANA_GIT=https://github.com/ECP-copa/Cabana.git
-CABANA_GIT=https://github.com/JDTruj2018/Cabana.git
+#KOKKOS_HASH=785d19f
+CABANA_GIT=https://github.com/ECP-copa/Cabana.git
+#CABANA_GIT=https://github.com/JDTruj2018/Cabana.git
 
 # Load Modules
-module load cmake-3.13.4-gcc-7.4.0-rdapt4e
-module load cuda-10.0.130-gcc-7.4.0-7svpedx
-module load gcc-7.4.0-gcc-8.1.0-j26pfmd
-#module load openmpi-3.1.3-gcc-7.4.0-ts2kdgn
-module load openmpi-3.1.4-gcc-9.2.0-imwbtvi
+# module purge
+module load gcc/9
+module load cuda/11.0.2-lqua
+module load cmake
+module load openmpi
 
 # Environment Variables for NVCC
-export NVCC_WRAPPER_DEFAULT_COMPILER=/opt/spack/opt/spack/linux-scientific7-x86_64/gcc-8.1.0/gcc-7.4.0-j26pfmdodybas2fpybqyi7hfvbc6kqot/bin/g++
+export NVCC_WRAPPER_DEFAULT_COMPILER=/opt/spack/opt/spack/linux-centos7-haswell/gcc-4.8.5/gcc-9.3.0-oxjixak2sy5mudydbjatzvj4bpveh5jl/bin/gcc
 export OMPI_CXX=${NVCC_CXX}
 
 # Abnormal Exit Message
@@ -47,11 +47,11 @@ build_silo() {
 # Build Kokkos on Xena (Tesla K40 GPU - KEPLER 35) with Cuda UVM
 build_kokkos() {
     cd ${MYDIR}/libs/kokkos
-    git checkout ${KOKKOS_HASH}
+    #git checkout ${KOKKOS_HASH}
     rm -rf build
     mkdir -p build
     cd build
-    cmake -D CMAKE_CXX_FLAGS="--expt-relaxed-constexpr" -D CMAKE_CXX_COMPILER=${NVCC_CXX} -D Kokkos_ARCH_KEPLER35=On -D Kokkos_ENABLE_CUDA_LAMBDA=On -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On -D Kokkos_ENABLE_CUDA=On ..
+    cmake -D CMAKE_CXX_FLAGS="--expt-relaxed-constexpr" -D CMAKE_CXX_COMPILER=${NVCC_CXX} -D Kokkos_ENABLE_CUDA_LAMBDA=On -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On -D Kokkos_ENABLE_CUDA=On -D Kokkos_CXX_STANDARD=14 ..
     make DESTDIR=${INSTALL_DIR} install -j8
     cd ${MYDIR}
 }
@@ -62,8 +62,8 @@ build_cabana() {
     rm -rf build
     mkdir -p build
     cd build
-    cmake -D CMAKE_PREFIX_PATH=${INSTALL_DIR}/usr/local -D CMAKE_CXX_COMPILER=${NVCC_CXX} -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On -D Kokkos_ENABLE_CUDA=On -D Cabana_ENABLE_CAJITA=On -D Cabana_ENABLE_MPI=On -D Cabana_REQUIRE_CUDA=On ..
-    make DESTDIR=${INSTALL_DIR} install -j8
+    cmake -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_PREFIX_PATH=${INSTALL_DIR}/usr/local -D CMAKE_INSTALL_PREFIX=${INSTALL_DIR} -D CMAKE_CXX_COMPILER=${NVCC_CXX} -D Cabana_REQUIRE_OPNEMP=ON -D Cabana_ENABLE_EXAMPLES=ON -D Cabana_ENABLE_TESTING=ON -D Cabana_ENABLE_PERFORMANCE_TESTING=ON -D Cabana_ENABLE_CAJITA=On -D Cabana_ENABLE_MPI=On -D Cabana_REQUIRE_CUDA=On ..
+    make install -j8
     cd ${MYDIR}
 }
 
@@ -162,12 +162,13 @@ done
 # Default Operation
 build_workspace
 
-# Build ExaCLAMR on Wheeler
+# Build ExaCLAMR on Xena
 cd ${MYDIR}
 rm -rf build
 mkdir -p build
 cd build
-cmake -D CMAKE_CXX_FLAGS=-I"${INSTALL_DIR}/include --expt-relaxed-constexpr"  -D CMAKE_PREFIX_PATH="${INSTALL_DIR}/usr/local;${INSTALL_DIR}/lib" -D CMAKE_CXX_COMPILER=${NVCC_CXX} -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On -D Kokkos_ENABLE_CUDA=On ..
+cmake -D CMAKE_CXX_FLAGS=-I"${INSTALL_DIR}/include --expt-relaxed-constexpr"  -D CMAKE_PREFIX_PATH="${INSTALL_DIR}/usr/local;${INSTALL_DIR}/lib;${INSTALL_DIR}/lib64/cmake/Cabana" -D CMAKE_CXX_COMPILER=${NVCC_CXX} -D Kokkos_ENABLE_OPENMP=On -D Kokkos_ENABLE_SERIAL=On -D Kokkos_ENABLE_CUDA=On ..
 make -j8
 mkdir data
 mkdir data/raw
+
